f59b4c5f59dc3bbcb59ae5ff776d6a08db6e60b8
sahetab <saheta.balleda@kony.com>
2018-01-23 15:28:09 +0530

    Revereted the platform related changes in BaseTest.java

--- test/TestNG/src/test/java/com/kony/appiumtests/tests/BaseTest.java
+++ test/TestNG/src/test/java/com/kony/appiumtests/tests/BaseTest.java
@@ -51,23 +51,31 @@ public abstract class BaseTest {
 		DesiredCapabilities capabilities = new DesiredCapabilities();
 		// The commented section is used when try to work in local Environment.
 
-		
-		//Capabilities for Android platform
-		/*  capabilities.setCapability("deviceName", "4d00962cd43212ad");
-		  capabilities.setCapability("platformVersion","4.4.2");
-		  capabilities.setCapability("platformName", "Android" );
-		  capabilities.setCapability("appPackage", "com.orgname.NovartisDemo");
-		  capabilities.setCapability("appActivity",
-		 "com.orgname.NovartisDemo.KssampleAG");
+		/*
+		 * capabilities.setCapability("deviceName", "4d002205f1d230af");
+		 * capabilities.setCapability("platformVersion","5.0.1");
+		 * capabilities.setCapability("platformName", "Android" );
+		 * capabilities.setCapability("appPackage", "com.engie.SampleTest");
+		 * capabilities.setCapability("appActivity",
+		 * "com.engie.SampleTest.SampleTest");
 		 */
 
-		  platformName = capabilities.getCapability("platformName").toString();
+		if (iosdriver == null) {
+
+			// Comm with Appium server
+			iosdriver = new IOSDriver(new URL("http://127.0.0.1:4723/wd/hub"),capabilities);
+		}
+
+		System.out.println("Getting platform name from capabilities.........."
+				+ iosdriver.getCapabilities().getPlatform().toString());
+		/**
+		 * getPlatform() is used to know the platform on which the app is
+		 * running
+		 */
+		platformName = iosdriver.getCapabilities().getPlatform().toString();
 
 		if ("MAC".equalsIgnoreCase(platformName)) {
 			System.out.println("Inside platform MAC............");
-			if (iosdriver == null) {
-				iosdriver = new IOSDriver(new URL("http://127.0.0.1:4723/wd/hub"),capabilities);
-			}
 			driver = iosdriver;
 		} else {
 			if (driver != null) {
